b80c097a7e4e59b7531c4de4ea4462e0e81b3208
jerrita <je5r1ta@icloud.com>
2026-05-20 10:41:43 +0800

    u

--- lua/plugins/lsp/treesitter.lua
+++ lua/plugins/lsp/treesitter.lua
@@ -1,17 +1,22 @@
 return {
     { -- Highlight
         "nvim-treesitter/nvim-treesitter",
-        branch = "master",
+        branch = "main",
         build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
-
-            configs.setup({
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "cpp", "python", "rust", "markdown" },
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end
+        main = "nvim-treesitter",
+        init = function()
+            local ensure_installed = { "c", "lua", "vim", "vimdoc", "cpp", "python", "rust", "markdown" }
+            local already_installed = require("nvim-treesitter.config").get_installed()
+            local parsers_to_install = vim.iter(ensure_installed)
+                :filter(function(parser)
+                    return not vim.tbl_contains(already_installed, parser)
+                end)
+                :totable()
+            require("nvim-treesitter").install(parsers_to_install)
+        end,
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+        },
     },
 }
